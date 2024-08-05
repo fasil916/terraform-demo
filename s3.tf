efc12963752731768fa2b9827c526ead673e197b
fasil916 <71936145+fasil916@users.noreply.github.com>
2024-08-05 12:57:26 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,19 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+  description = "The AWS access key ID"
+  sensitive = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+  description = "The AWS secret access key"
+  sensitive = true
+}
 
 provider "aws" {
   region  = "us-east-1"  # Set your preferred AWS region
+access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 resource "aws_s3_bucket" "example" {
   bucket = "fazzzzz"
